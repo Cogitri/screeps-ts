be94058ebe768772e847f1d31ca4a81417cbc004
katharina.d.sprotte <katharina.d.sprotte@student.fh-kiel.de>
2021-08-02 12:24:27 +0200

    solved linter

--- src/core/creepSpawn.ts
+++ src/core/creepSpawn.ts
@@ -1,16 +1,12 @@
 export default function (spawn: StructureSpawn): void {
   // Spawn a creep if there is none
-  var creepNumber = 0;
+  let creepNumber = 0;
   if (!spawn.room.find(FIND_MY_CREEPS).length) {
-
-
     const creepBasicBody = [MOVE];
-     var creepName = 'Creep' + creepNumber;
-     creepNumber++;
+    const creepName = `Creep${creepNumber}`;
+    creepNumber++;
 
-     spawn.spawnCreep([WORK,CARRY,MOVE], creepName,
-       {memory: {role: 'creep', room:'', working:true}});
+    spawn.spawnCreep([WORK, CARRY, MOVE], creepName, { memory: { role: "creep", room: "", working: true } });
     return;
   }
-
 }
