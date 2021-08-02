a2f06f3c5dbf3a71ecbb6a51cea82232aff0c880
katharina.d.sprotte <katharina.d.sprotte@student.fh-kiel.de>
2021-08-02 11:49:21 +0200

    assign name for creep

--- src/core/creepSpawn.ts
+++ src/core/creepSpawn.ts
@@ -1,9 +1,16 @@
 export default function (spawn: StructureSpawn): void {
   // Spawn a creep if there is none
+  var creepNumber = 0;
   if (!spawn.room.find(FIND_MY_CREEPS).length) {
     // TODO: Implement defined standard creep body (Issue 4)
-    // TODO: Assing unique name (Issue 5)
-    spawn.spawnCreep([MOVE], `Creep1`);
+
+    // assigning unique name (Issue 5)
+    var creepName = 'Creep' + creepNumber;
+    creepNumber++;
+
+    spawn.spawnCreep([WORK,CARRY,MOVE], creepName,
+      {memory: {role: 'creep', room:'', working:true}});
     return;
   }
+
 }
