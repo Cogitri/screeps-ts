a295ab5c5633177e53ff217aabe590a315c7561e
unknown <lara.laskowsky@student.fh-kiel.de>
2021-08-02 11:34:00 +0200

    Add screeps-body for basic screeps

--- src/core/creepSpawn.ts
+++ src/core/creepSpawn.ts
@@ -1,9 +1,9 @@
 export default function (spawn: StructureSpawn): void {
   // Spawn a creep if there is none
   if (!spawn.room.find(FIND_MY_CREEPS).length) {
-    // TODO: Implement defined standard creep body (Issue 4)
+    const creepBasicBody = [MOVE];
     // TODO: Assing unique name (Issue 5)
-    spawn.spawnCreep([MOVE], `Creep1`);
+    spawn.spawnCreep(creepBasicBody, `Creep1`);
     return;
   }
 }
