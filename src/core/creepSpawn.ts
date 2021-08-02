e8d08e8a6106866850452c54f858093ad455f4c7
katharina.d.sprotte <katharina.d.sprotte@student.fh-kiel.de>
2021-08-02 12:28:57 +0200

    solved lint

--- src/core/creepSpawn.ts
+++ src/core/creepSpawn.ts
@@ -2,11 +2,11 @@ export default function (spawn: StructureSpawn): void {
   // Spawn a creep if there is none
   let creepNumber = 0;
   if (!spawn.room.find(FIND_MY_CREEPS).length) {
-    const creepBasicBody = [MOVE];
+    const creepBasicBody = [WORK, CARRY, MOVE];
     const creepName = `Creep${creepNumber}`;
     creepNumber++;
 
-    spawn.spawnCreep([WORK, CARRY, MOVE], creepName, { memory: { role: "creep", room: "", working: true } });
+    spawn.spawnCreep(creepBasicBody, creepName, { memory: { role: "creep", room: "", working: true } });
     return;
   }
 }
