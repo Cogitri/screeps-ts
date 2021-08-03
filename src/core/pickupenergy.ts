d43de03d2726db9a7cab470c7a113cb6de4aa931
janis.j.h.ciemnyjewski <janis.j.h.ciemnyjewski@student.fh-kiel.de>
2021-08-03 11:31:18 +0200

    #17 Energie kann vom Boden, Tomstones und Ruinen aufgenommen werden

--- src/core/pickupenergy.ts
+++ src/core/pickupenergy.ts
@@ -1,6 +1,36 @@
 export default function(): void{
     for (const name in Game.creeps) {
         const creep = Game.creeps[name];
-        var droppedResources = creep.room.find(FIND_DROPPED_RESOURCES)
+
+        var droppedEnergy = creep.room.find(FIND_DROPPED_RESOURCES, {
+            filter: (r) => r.resourceType == RESOURCE_ENERGY
+       });
+       var ruin = creep.room.find(FIND_RUINS);
+       var tombstones = creep.room.find(FIND_TOMBSTONES);
+       console.log('pickupEnergy läuft');
+
+        if (droppedEnergy.length) {
+            creep.moveTo(droppedEnergy[0]);
+            var pickupDropped =      creep.pickup(droppedEnergy[0]);
+            console.log(pickupDropped);
+        }
+
+        //var lootRuin = creep.store[RESOURCE_ENERGY];
+        var lootRuin =creep.withdraw(ruin[0], RESOURCE_ENERGY);
+        if (ruin.length) {
+            console.log();
+            console.log('ruin');
+            //console.log(creep.store[RESOURCE_ENERGY]);
+        }
+        if(lootRuin == ERR_NOT_IN_RANGE){
+            creep.moveTo(ruin[0]);
+        }
+
+        if(tombstones.length){
+            creep.moveTo(tombstones[0]);
+            var lootTombstone = creep.withdraw(tombstones[0], RESOURCE_ENERGY);
+            console.log(lootTombstone);
+            console.log(creep.withdraw(tombstones[0], RESOURCE_ENERGY));
+        }
     }
 }
