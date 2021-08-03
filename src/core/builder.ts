9239620fad7fc5047a7cc6b2d80b81fd705cd6fd
mattis.kunstmann <mattis.kunstmann@student.fh-kiel.de>
2021-08-03 08:52:24 +0000

    fixed typo

--- src/core/builder.ts
+++ src/core/builder.ts
@@ -1,7 +1,7 @@
 const body = [WORK, MOVE, MOVE, CARRY, CARRY];
 let builderNumber = 0;
 
-export function spwanBuilder(spawn: StructureSpawn): void {
+export function spawnBuilder(spawn: StructureSpawn): void {
   const name = `builder${builderNumber}`;
   builderNumber++;
   spawn.spawnCreep(body, name, { memory: { role: "builder", room: "", working: false, counter: 34 } });
