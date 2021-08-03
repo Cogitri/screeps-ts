9b078f933422315adb2f71acd7e593608fb4b170
katharina.d.sprotte <katharina.d.sprotte@student.fh-kiel.de>
2021-08-03 12:10:23 +0200

    #15: check für anzahl harvester und builder und wenn notwendig neue spawnen hinzugefügt fixes #15

--- src/core/gameLoop.ts
+++ src/core/gameLoop.ts
@@ -1,5 +1,7 @@
-import creepSpawn from "./creepSpawn";
+import { spawnHarvester } from "../creeps/models/modelHarvester";
 import creepWork from "./creepWork";
+import creepSpawn from "./creepSpawn";
+import { spawnBuilder } from "../creeps/models/modelBuilder";
 
 export default function (): void {
   // Iterate over all owned spawns
@@ -7,6 +9,17 @@ export default function (): void {
     // Check if creep is already spawning (avoids bug)
     if (!Game.spawns[spawn].spawning) {
       creepSpawn(Game.spawns[spawn]);
+    }
+
+    const builders = _.filter(Game.creeps, creep => creep.memory.role === "builder");
+    // check number of creeps and check if creep is already spawning (avoids bug)
+    if (builders.length < 3 && !Game.spawns[spawn].spawning) {
+      spawnBuilder(Game.spawns[spawn]);
+    }
+    const harvesters = _.filter(Game.creeps, creep => creep.memory.role === "harvester");
+    // check number of creeps and check if creep is already spawning (avoids bug)
+    if (harvesters.length < 3 && !Game.spawns[spawn].spawning) {
+      spawnHarvester(Game.spawns[spawn]);
     }
   }
 
