35c2fa0776bda44c2d1c6fa4e4e06e8243b5e987
katharina.d.sprotte <katharina.d.sprotte@student.fh-kiel.de>
2021-08-03 14:38:03 +0200

    #15: an neue Ordnerstruktur angepasst
    fixes #15

--- src/core/creepSpawn.ts
+++ src/core/creepSpawn.ts
@@ -1,3 +1,6 @@
+import { spawnBuilder } from ".././creeps/models/modelBuilder";
+import { spawnHarvester } from ".././creeps/models/modelHarvester";
+
 export default function (spawn: StructureSpawn): void {
   // Spawn a creep if there is none
   let creepNumber = 0;
@@ -8,5 +11,15 @@ export default function (spawn: StructureSpawn): void {
 
     spawn.spawnCreep(creepBasicBody, creepName, { memory: { role: "creep", room: "", working: false } });
     return;
+  }
+  const builders = _.filter(Game.creeps, creep => creep.memory.role === "builder");
+  // check number of creeps and check if creep is already spawning (avoids bug)
+  if (builders.length < 3) {
+    spawnBuilder(spawn);
+  }
+  const harvesters = _.filter(Game.creeps, creep => creep.memory.role === "harvester");
+  // check number of creeps and check if creep is already spawning (avoids bug)
+  if (harvesters.length < 3) {
+    spawnHarvester(spawn);
   }
 }
