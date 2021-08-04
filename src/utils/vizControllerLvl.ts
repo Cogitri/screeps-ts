c1337df233f8e8a3ef3ca95dfd0acc931ba2c1d9
pvoss <paul.voss@student.fh-kiel.de>
2021-08-04 13:08:40 +0200

    #53 - made linter happy

--- src/utils/vizControllerLvl.ts
+++ src/utils/vizControllerLvl.ts
@@ -1,8 +1,13 @@
-export function visualizeControllerProgress(controller: StructureController) :void {
-    const roomController = controller;
-    const x_Coordinate = roomController.pos.x;
-    const y_Coordinate = roomController.pos.y;
-    const totalToUpgrade = roomController.progress + roomController.progressTotal; 
+export function visualizeControllerProgress(controller: StructureController): void {
+  const roomController = controller;
+  const xCoordinate = roomController.pos.x;
+  const yCoordinate = roomController.pos.y;
+  const totalToUpgrade = roomController.progress + roomController.progressTotal;
 
-    new RoomVisual().text(`${(roomController.progress / totalToUpgrade * 100).toFixed(2)}% to next Level`, x_Coordinate, y_Coordinate, {color: 'white', font: 0.8});
+  new RoomVisual().text(
+    `${((roomController.progress / totalToUpgrade) * 100).toFixed(2)}% to next Level`,
+    xCoordinate,
+    yCoordinate,
+    { color: "white", font: 0.8 }
+  );
 }
