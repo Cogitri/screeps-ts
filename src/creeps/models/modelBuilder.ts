aa5e6fb006073606b7fb7d0008d54510a82994e2
Rasmus Thomsen <oss@cogitri.dev>
2021-08-04 14:47:30 +0200

    Fehlendes "target" zu models hinzugefügt.

--- src/creeps/models/modelBuilder.ts
+++ src/creeps/models/modelBuilder.ts
@@ -2,5 +2,7 @@ const body = [WORK, MOVE, MOVE, CARRY, CARRY];
 
 export function spawnBuilder(spawn: StructureSpawn): void {
   const name = `builder${Game.time}${Math.trunc(Math.random() * 10)}`;
-  spawn.spawnCreep(body, name, { memory: { role: "builder", room: "", working: false, lockTask: false } });
+  spawn.spawnCreep(body, name, {
+    memory: { role: "builder", room: "", working: false, lockTask: false, target: null }
+  });
 }
--- src/creeps/models/modelSoldier.ts
+++ src/creeps/models/modelSoldier.ts
@@ -2,5 +2,7 @@ const body = [WORK, CARRY, MOVE, ATTACK, TOUGH, TOUGH];
 
 export function spawnSoldier(spawn: StructureSpawn): void {
   const name = `soldier${Game.time}${Math.trunc(Math.random() * 10)}`;
-  spawn.spawnCreep(body, name, { memory: { role: "soldier", room: "", working: false, lockTask: false } });
+  spawn.spawnCreep(body, name, {
+    memory: { role: "soldier", room: "", working: false, lockTask: false, target: null }
+  });
 }
