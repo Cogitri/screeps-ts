73f2e40702a0d0acd2b4e67b939b9c217619b17f
thorben.rolf <thorben.rolf@student.fh-kiel.de>
2021-08-04 09:12:34 +0000

    Soldier Nachricht angepasst

--- src/creeps/routines/routineSoldier.ts
+++ src/creeps/routines/routineSoldier.ts
@@ -26,6 +26,6 @@ export default function (creep: Creep): void {
 
   if (creep.attack(enemy) === ERR_NOT_IN_RANGE) {
     creep.moveTo(enemy, { visualizePathStyle: { stroke: pathColor } });
-    creep.say("⚔️ deliver");
+    creep.say("⚔️ attack");
   }
 }
