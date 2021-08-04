ece3e56e691483950f92571b82834ff766ee7b5e
katharina.d.sprotte <katharina.d.sprotte@student.fh-kiel.de>
2021-08-04 13:07:54 +0200

    #37 added pathcolor

--- src/creeps/routines/routineEnergizeTower.ts
+++ src/creeps/routines/routineEnergizeTower.ts
@@ -1,10 +1,11 @@
 export default function (creep: Creep): void {
+  const pathColor = "#33d6ff";
   const tower = creep.room.find(FIND_STRUCTURES, {
     filter: structure => structure.structureType === STRUCTURE_TOWER
   });
 
   if (creep.transfer(tower[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-    creep.say("⛏ transfer");
-    creep.moveTo(tower[0]);
+    creep.say("transfer");
+    creep.moveTo(tower[0], { visualizePathStyle: { stroke: pathColor } });
   }
 }
