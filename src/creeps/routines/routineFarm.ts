1ca0334c9d206131143d9ad983a3779fb96560d7
katharina.d.sprotte <katharina.d.sprotte@student.fh-kiel.de>
2021-08-04 12:59:56 +0200

    #37 added tower energizing closes #37

--- src/creeps/routines/routineFarm.ts
+++ src/creeps/routines/routineFarm.ts
@@ -2,7 +2,7 @@ export default function (creep: Creep): void {
   const pathColor = "#ffff33";
 
   const sources = creep.room.find(FIND_SOURCES);
-  if (creep.harvest(sources[1]) === ERR_NOT_IN_RANGE) {
+  if (creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
     creep.say("⛏ harvest");
     creep.moveTo(sources[1], { visualizePathStyle: { stroke: pathColor } });
   } else if (creep.harvest(sources[0]) === OK) {
