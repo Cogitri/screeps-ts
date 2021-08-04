033f0e5455ca3f4f4ec546654e1520ae73ecc283
dennis.schuetz <dennis.schuetz@student.fh-kiel.de>
2021-08-04 12:19:02 +0000

    hand for withdraw symbol added

--- src/creeps/routines/routineWithdraw.ts
+++ src/creeps/routines/routineWithdraw.ts
@@ -5,7 +5,7 @@ export default function (creep: Creep): void {
   });
 
   if (creep.withdraw(containers[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-    creep.say("withdraw");
+    creep.say("🖐︎ withdraw");
     creep.moveTo(containers[0], { visualizePathStyle: { stroke: pathColor } });
   }
 }
