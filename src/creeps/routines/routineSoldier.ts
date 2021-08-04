787ac3762549631709c8279529aeb353925d7dd5
Lara Laskowsky <lara.laskowsky@student.fh-kiel.de>
2021-08-04 10:26:29 +0200

    #51: Add pathcolor to soldier

--- src/creeps/routines/routineSoldier.ts
+++ src/creeps/routines/routineSoldier.ts
@@ -1,4 +1,6 @@
 export default function (creep: Creep): void {
+  const pathColor = "#ff333";
+
   if (creep.memory.working && creep.store[RESOURCE_ENERGY] === 0) {
     creep.memory.working = false;
   }
@@ -23,6 +25,6 @@ export default function (creep: Creep): void {
   }
 
   if (creep.attack(enemy) === ERR_NOT_IN_RANGE) {
-    creep.moveTo(enemy);
+    creep.moveTo(enemy, { visualizePathStyle: { stroke: pathColor } });
   }
 }
