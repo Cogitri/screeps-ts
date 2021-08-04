46766f5dae16c7f6a7f456926fc2d91ec2adb0f6
Rasmus Thomsen <oss@cogitri.dev>
2021-08-04 16:42:48 +0200

    Fehlendes Attribut 'target' zu modelTransporter hinzugefügt

--- src/creeps/models/modelTransporter.ts
+++ src/creeps/models/modelTransporter.ts
@@ -2,5 +2,7 @@ const body = [WORK, MOVE, MOVE, CARRY, CARRY];
 
 export function spawnTransporter(spawn: StructureSpawn): void {
   const name = `transporter${Game.time}${Math.trunc(Math.random() * 10)}`;
-  spawn.spawnCreep(body, name, { memory: { role: "transporter", room: "", working: false, lockTask: false } });
+  spawn.spawnCreep(body, name, {
+    memory: { role: "transporter", room: "", working: false, lockTask: false, target: null }
+  });
 }
