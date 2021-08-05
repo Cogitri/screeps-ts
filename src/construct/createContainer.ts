620b80d8de1da0e6722bf285617a55bef43d99e8
janis.j.h.ciemnyjewski <janis.j.h.ciemnyjewski@student.fh-kiel.de>
2021-08-05 11:50:14 +0200

    #31 now working and only processed if not enough Containers

--- src/construct/createContainer.ts
+++ src/construct/createContainer.ts
@@ -1,99 +1,126 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-for-in-array */
 export default function (spawn: StructureSpawn): void {
   // Container placement here
+  // Amount of Containers that should be built
+  const c = 0; // CHANGE HERE
+
   const energySources = spawn.room.find(FIND_SOURCES_ACTIVE);
-  for (const i in energySources) {
-    const position = energySources[i].pos;
-    const xSource: number = position.x;
-    const ySource: number = position.y;
-    const terrain = new Room.Terrain(spawn.room.name);
-    const xArray: number[] = [-1, -1, -1, 0, 0, 1, 1, 1];
-    const yArray: number[] = [-1, 0, 1, -1, 1, -1, 0, 1];
-    const xPositionsAvailable: number[] = [];
-    const yPositionsAvailable: number[] = [];
-    // checks if the spaces right next to the source are plain/empty
-    for (let l = 0; l < 8; l++) {
-      const xCheck = xSource + xArray[l];
-      const yCheck = ySource + yArray[l];
-      if (terrain.get(xCheck, yCheck) === 0) {
-        xPositionsAvailable.push(xCheck);
-        yPositionsAvailable.push(yCheck);
+  for (const source of energySources) {
+    const xSource: number = source.pos.x;
+    const ySource: number = source.pos.y;
+
+    // Checks if there are any Construction Sites for Containers or Containers already there
+    const checkConstructions = source.pos.findInRange(FIND_MY_CONSTRUCTION_SITES, 2, {
+      filter: n => n.structureType === STRUCTURE_CONTAINER
+    });
+    const checkContainer = source.pos.findInRange(FIND_STRUCTURES, 2, {
+      filter: n => n.structureType === STRUCTURE_CONTAINER
+    });
+
+    // If the amount of Containers is too low, the process of placing them begins
+    if (checkConstructions.length <= c && checkContainer.length <= c) {
+      const terrain = new Room.Terrain(spawn.room.name);
+      const xArray: number[] = [-1, -1, -1, 0, 0, 1, 1, 1];
+      const yArray: number[] = [-1, 0, 1, -1, 1, -1, 0, 1];
+      const xPositionsAvailable: number[] = [];
+      const yPositionsAvailable: number[] = [];
+
+      // checks if the spaces right next to the source are plain/empty
+      for (let l = 0; l < 8; l++) {
+        if (terrain.get(xSource - xArray[l], ySource - yArray[l]) !== 1) {
+          xPositionsAvailable.push(xSource - xArray[l]);
+          yPositionsAvailable.push(ySource - yArray[l]);
+        }
       }
-    }
-    const xBuildPlaceAvailable: number[] = [];
-    const yBuildPlaceAvailable: number[] = [];
-    // checks for all available positions if there's room to place a container
-    for (const k of xPositionsAvailable) {
-      switch (xPositionsAvailable[k] - xSource) {
-        case -1:
-          switch (yPositionsAvailable[k] - ySource) {
-            case -1:
-              if (terrain.get(xPositionsAvailable[k] - 1, yPositionsAvailable[k] - 1) === 0) {
-                xBuildPlaceAvailable.push(xPositionsAvailable[k] - 1);
-                yBuildPlaceAvailable.push(yPositionsAvailable[k] - 1);
-              }
-              break;
-            case 1:
-              if (terrain.get(xPositionsAvailable[k] - 1, yPositionsAvailable[k] - 1) === 0) {
-                xBuildPlaceAvailable.push(xPositionsAvailable[k] - 1);
-                yBuildPlaceAvailable.push(yPositionsAvailable[k] + 1);
-              }
-              break;
-            default:
-              if (terrain.get(xPositionsAvailable[k] - 1, yPositionsAvailable[k] - 1) === 0) {
-                xBuildPlaceAvailable.push(xPositionsAvailable[k] - 1);
-                yBuildPlaceAvailable.push(yPositionsAvailable[k]);
-              }
-              break;
-          }
-          break;
-        case 1:
-          switch (yPositionsAvailable[k] - ySource) {
-            case -1:
-              if (terrain.get(xPositionsAvailable[k] - 1, yPositionsAvailable[k] - 1) === 0) {
-                xBuildPlaceAvailable.push(xPositionsAvailable[k] + 1);
-                yBuildPlaceAvailable.push(yPositionsAvailable[k] - 1);
-              }
-              break;
-            case 1:
-              if (terrain.get(xPositionsAvailable[k] - 1, yPositionsAvailable[k] - 1) === 0) {
-                xBuildPlaceAvailable.push(xPositionsAvailable[k] + 1);
-                yBuildPlaceAvailable.push(yPositionsAvailable[k] + 1);
-              }
-              break;
-            default:
-              if (terrain.get(xPositionsAvailable[k] - 1, yPositionsAvailable[k] - 1) === 0) {
-                xBuildPlaceAvailable.push(xPositionsAvailable[k] + 1);
-                yBuildPlaceAvailable.push(yPositionsAvailable[k]);
-              }
-              break;
-          }
-          break;
-        default:
-          switch (yPositionsAvailable[k] - ySource) {
-            case -1:
-              if (terrain.get(xPositionsAvailable[k] - 1, yPositionsAvailable[k] - 1) === 0) {
-                xBuildPlaceAvailable.push(xPositionsAvailable[k]);
-                yBuildPlaceAvailable.push(yPositionsAvailable[k] - 1);
-              }
-              break;
-            case 1:
-              if (terrain.get(xPositionsAvailable[k] - 1, yPositionsAvailable[k] - 1) === 0) {
-                xBuildPlaceAvailable.push(xPositionsAvailable[k]);
-                yBuildPlaceAvailable.push(yPositionsAvailable[k] + 1);
-              }
-              break;
-            default:
-              // Not possible/ it's position of the energy source
-              break;
-          }
-          break;
+      const xBuildPlaceAvailable: number[] = [];
+      const yBuildPlaceAvailable: number[] = [];
+
+      // checks for all available positions if there's room to place a container
+      for (const k in xPositionsAvailable) {
+        // X-Axis
+        switch (xPositionsAvailable[k] - xSource) {
+          case -1:
+            // Y-Axis
+            switch (yPositionsAvailable[k] - ySource) {
+              case -1:
+                // Checks, if it is possible to build at the Position
+                if (terrain.get(xPositionsAvailable[k] - 1, yPositionsAvailable[k] - 1) !== 1) {
+                  xBuildPlaceAvailable.push(xPositionsAvailable[k] - 1);
+                  yBuildPlaceAvailable.push(yPositionsAvailable[k] - 1);
+                }
+                break;
+              case 1:
+                if (terrain.get(xPositionsAvailable[k] - 1, yPositionsAvailable[k] - 1) !== 1) {
+                  xBuildPlaceAvailable.push(xPositionsAvailable[k] - 1);
+                  yBuildPlaceAvailable.push(yPositionsAvailable[k] + 1);
+                }
+                break;
+              default:
+                if (terrain.get(xPositionsAvailable[k] - 1, yPositionsAvailable[k] - 1) !== 1) {
+                  xBuildPlaceAvailable.push(xPositionsAvailable[k] - 1);
+                  yBuildPlaceAvailable.push(yPositionsAvailable[k]);
+                }
+                break;
+            }
+            break;
+          case 1:
+            switch (yPositionsAvailable[k] - ySource) {
+              case -1:
+                if (terrain.get(xPositionsAvailable[k] - 1, yPositionsAvailable[k] - 1) !== 1) {
+                  xBuildPlaceAvailable.push(xPositionsAvailable[k] + 1);
+                  yBuildPlaceAvailable.push(yPositionsAvailable[k] - 1);
+                }
+                break;
+              case 1:
+                if (terrain.get(xPositionsAvailable[k] - 1, yPositionsAvailable[k] - 1) !== 1) {
+                  xBuildPlaceAvailable.push(xPositionsAvailable[k] + 1);
+                  yBuildPlaceAvailable.push(yPositionsAvailable[k] + 1);
+                }
+                break;
+              default:
+                if (terrain.get(xPositionsAvailable[k] - 1, yPositionsAvailable[k] - 1) !== 1) {
+                  xBuildPlaceAvailable.push(xPositionsAvailable[k] + 1);
+                  yBuildPlaceAvailable.push(yPositionsAvailable[k]);
+                }
+                break;
+            }
+            break;
+          default:
+            switch (yPositionsAvailable[k] - ySource) {
+              case -1:
+                if (terrain.get(xPositionsAvailable[k] - 1, yPositionsAvailable[k] - 1) !== 1) {
+                  xBuildPlaceAvailable.push(xPositionsAvailable[k]);
+                  yBuildPlaceAvailable.push(yPositionsAvailable[k] - 1);
+                }
+                break;
+              case 1:
+                if (terrain.get(xPositionsAvailable[k] - 1, yPositionsAvailable[k] - 1) !== 1) {
+                  xBuildPlaceAvailable.push(xPositionsAvailable[k]);
+                  yBuildPlaceAvailable.push(yPositionsAvailable[k] + 1);
+                }
+                break;
+              default:
+                // Not possible, because it's position of the energy source
+                console.log("Container placement has reached a position it shouldn't");
+                break;
+            }
+            break;
+        }
       }
-    }
-    // Building the containers. for now only one is built per energy source.
-    if (xBuildPlaceAvailable.length > 0) {
-      if (terrain.get(xBuildPlaceAvailable[0], yBuildPlaceAvailable[0]) === 0) {
-        Game.rooms.spawn.createConstructionSite(xBuildPlaceAvailable[0], yBuildPlaceAvailable[0], STRUCTURE_CONTAINER);
+      // Building the containers. for now only one is built per energy source.
+
+      if (xBuildPlaceAvailable.length > c) {
+        if (terrain.get(xBuildPlaceAvailable[c], yBuildPlaceAvailable[c]) !== 1) {
+          spawn.room.createConstructionSite(xBuildPlaceAvailable[c], yBuildPlaceAvailable[c], STRUCTURE_CONTAINER);
+          console.log(
+            "Placed a Container Construction site at: ",
+            xPositionsAvailable[c],
+            ", ",
+            yPositionsAvailable[c]
+          );
+        }
       }
     }
   }
