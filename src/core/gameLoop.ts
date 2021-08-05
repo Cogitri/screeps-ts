8a2c29dea9165650a6a0be8dfed7041efb7f477c
Lara Laskowsky <lara.laskowsky@student.fh-kiel.de>
2021-08-05 13:43:47 +0200

    Refactoring code and add comment to calculation

--- src/core/gameLoop.ts
+++ src/core/gameLoop.ts
@@ -18,12 +18,10 @@ export default function (): void {
     const spawningProcess = Game.spawns[spawn].spawning;
     if (spawningProcess != null) {
       const spawningCreep = Game.creeps[spawningProcess.name];
-      // TODO: Prozent des abgeschlossenen Spawning-Prozesses
 
-      const timeNeededInTotal = spawningProcess.needTime;
-      const timeRemaining = spawningProcess.remainingTime;
-      const timeLeft = timeNeededInTotal - timeRemaining;
-      const percentDone = (100 / timeNeededInTotal) * timeLeft;
+      // Calculate percentage of the time that already passed
+      const timeLeft = spawningProcess.needTime - spawningProcess.remainingTime;
+      const percentDone = (100 / spawningProcess.needTime) * timeLeft;
 
       Game.spawns[spawn].room.visual.text(
         "⏳ " + `${percentDone.toFixed(2)}%`,
@@ -32,29 +30,12 @@ export default function (): void {
         { align: "left", opacity: 0.8, font: 0.5 }
       );
 
-      // 🛠️
       Game.spawns[spawn].room.visual.text(
         "🧬 " + spawningProcess.name,
         Game.spawns[spawn].pos.x - 1.25,
         Game.spawns[spawn].pos.y + 2.25,
         { align: "left", opacity: 0.8, font: 0.5 }
       );
-      /*
-      Game.spawns[spawn].room.visual.text(
-        "⏳ " + `${percentDone.toFixed(2)}%`,
-        Game.spawns[spawn].pos.x + 1,
-        Game.spawns[spawn].pos.y,
-        { align: "left", opacity: 0.8, font: 0.5 }
-      );
-
-      // 🛠️
-      Game.spawns[spawn].room.visual.text(
-        "🧬" + spawningProcess.name,
-        Game.spawns[spawn].pos.x + 1,
-        Game.spawns[spawn].pos.y + 0.75,
-        { align: "left", opacity: 0.8, font: 0.5 }
-      );
-      */
     }
   }
 
