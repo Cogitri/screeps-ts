05313cbd051e45ee858c1e6818bee1f14413c3f6
Lara Laskowsky <lara.laskowsky@student.fh-kiel.de>
2021-08-05 09:20:10 +0200

    #52: Try to implement issue, but does not work

--- src/core/creepSpawn.ts
+++ src/core/creepSpawn.ts
@@ -1,3 +1,4 @@
+import { isNull } from "lodash";
 import { spawnBuilder } from ".././creeps/models/modelBuilder";
 import { spawnHarvester } from ".././creeps/models/modelHarvester";
 import { spawnSoldier } from ".././creeps/models/modelSoldier";
@@ -15,30 +16,64 @@ export default function (spawn: StructureSpawn): void {
     });
     return;
   }
+
   const builders = _.filter(Game.creeps, creep => creep.memory.role === "builder");
   // check number of creeps and check if creep is already spawning (avoids bug)
   if (builders.length < 3) {
     spawnBuilder(spawn);
   }
+
   const harvesters = _.filter(Game.creeps, creep => creep.memory.role === "harvester");
   // check number of creeps and check if creep is already spawning (avoids bug)
   if (harvesters.length < 3) {
     spawnHarvester(spawn);
   }
+
   const soldiers = _.filter(Game.creeps, creep => creep.memory.role === "soldier");
   // check number of creeps and check if creep is already spawning (avoids bug)
   if (soldiers.length < 1) {
     spawnSoldier(spawn);
   }
 
+  /*console.log(spawn.spawning);
+
+  console.log(!spawn.spawning);
+
+  console.log(!!spawn.spawning);*/
+  /*
+  console.log("!(spawn.spawning == null): " + !(spawn.spawning == null));
+
+  console.log("!(spawn.spawning === null): " + !(spawn.spawning === null));
+
+  console.log("!(spawn.spawning != null): " + !(spawn.spawning != null));
+
+  console.log("!(spawn.spawning !== null): " + !(spawn.spawning !== null));
+
+  console.log("spawn.spawning == null: " + spawn.spawning == null);
+
+  console.log("spawn.spawning === null: " + spawn.spawning === null);
+
+  console.log("spawn.spawning != null: " + spawn.spawning != null);
+
+  console.log("spawn.spawning !== null: " + spawn.spawning !== null);
+*/
+  /*
+  if(Game.spawns['Spawn1'].spawning) {
+    var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
+    Game.spawns['Spawn1'].room.visual.text(
+        '🛠️' + spawningCreep.memory.role,
+        Game.spawns['Spawn1'].pos.x + 1,
+        Game.spawns['Spawn1'].pos.y,
+        {align: 'left', opacity: 0.8});
+  }
+  */
+
+  console.log(spawn?.spawning);
+  console.log(spawn?.spawning != null);
+
   if (spawn.spawning) {
-    /*var spawningCreep = Game.creeps[spawn.spawning.name];
-    spawn.room.visual.text("🛠️" + spawningCreep.memory.role, spawn.pos.x + 1, spawn.pos.y, {
-      align: "left",
-      opacity: 0.8
-    });*/
     console.log("yay");
-    var spawningCreep = Game.creeps[spawn.spawning.name];
+    const spawningCreep = Game.creeps[spawn.spawning.name];
     spawn.room.visual.text("🛠️" + spawningCreep.memory.role, spawn.pos.x + 1, spawn.pos.y, {
       align: "left",
       opacity: 0.8
