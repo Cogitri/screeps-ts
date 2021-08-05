896132b2c8e4734c2a891bf4706e8c0dfbc38d6b
Dennis <dennis.schuetz@gmx.de>
2021-08-05 10:44:06 +0200

    Dennis-#20: fixed soldier not going to low prio task

--- src/creeps/routines/routineSoldier.ts
+++ src/creeps/routines/routineSoldier.ts
@@ -7,10 +7,6 @@ export default function (creep: Creep): void {
     creep.memory.working = false;
   }
 
-  if (!creep.memory.working && creep.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
-    creep.memory.working = true;
-  }
-
   // Find closest enemy alive
   // ! Can't find whether or not `FIND_HOSTILE_CREEPS` returns hostile PowerCreeps aswell
   const enemy: AnyCreep | null = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS, { filter: c => c.hits > 0 });
@@ -18,7 +14,7 @@ export default function (creep: Creep): void {
   // Return to fallback and go harvest when there are no enemies or the enemy is dead
   if (!enemy) {
     // TODO: Refer to harvesting routine or return energy to base
-    if (creep.memory.working) {
+    if (!creep.memory.working) {
       routineHarvester(creep);
     }
     return;
