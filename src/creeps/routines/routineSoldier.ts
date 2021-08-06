8f3c250bf55e27061ea21efc993718d33a57eb2e
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-08-06 11:55:23 +0200

    #70: make working state have sense

--- src/creeps/routines/routineSoldier.ts
+++ src/creeps/routines/routineSoldier.ts
@@ -6,20 +6,16 @@ const HP_THRESHOLD = 200;
 export default function (creep: Creep): void {
   const pathColor = "#ff3333";
 
-  if (creep.memory.working && creep.store[RESOURCE_ENERGY] === 0) {
-    creep.memory.working = false;
-  }
-
   // Find closest enemy alive
   // ! Can't find whether or not `FIND_HOSTILE_CREEPS` returns hostile PowerCreeps aswell
   const enemy: AnyCreep | null = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS, { filter: c => c.hits > 0 });
 
+  // double negation is needed as otherwise TS doesn't allow to assing `null` as a boolean
+  creep.memory.working = !!enemy;
+
   // Return to fallback and go harvest when there are no enemies or the enemy is dead
   if (!enemy) {
-    // TODO: Refer to harvesting routine or return energy to base
-    if (!creep.memory.working) {
-      routineTransporter(creep);
-    }
+    routineTransporter(creep);
     return;
   }
 
