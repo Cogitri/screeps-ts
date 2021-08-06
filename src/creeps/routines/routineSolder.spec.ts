19ccb93688c525c3bce666d40af638ddcddff2f4
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-08-06 11:18:57 +0200

    #70: refactor tests

--- src/creeps/routines/routineSolder.spec.ts
+++ src/creeps/routines/routineSolder.spec.ts
@@ -9,38 +9,16 @@ describe("Soldier role", () => {
       const enemy = mockInstanceOf<Creep>({
         my: false,
         hits: 1,
-        body: [
-          {
-            type: "attack",
-            hits: 100
-          },
-
-          {
-            type: "tough",
-            hits: 100
-          }
-        ],
+        body: composeBody(1),
         pos: {
           x: 100,
           y: 100
-        },
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        getActiveBodyparts: () => 5
+        }
       });
 
       const us = mockInstanceOf<Creep>({
-        body: [
-          {
-            type: "attack",
-            hits: 100
-          },
-          {
-            type: "tough",
-            hits: 100
-          }
-        ],
+        body: composeBody(2),
         hits: 10,
-        getActiveBodyparts: () => 10,
         memory: { role: "soldier", working: false },
         store: { getFreeCapacity: () => 50 },
         room: {
@@ -61,42 +39,88 @@ describe("Soldier role", () => {
       expect(us.moveTo).toHaveBeenCalledWith(enemy, { visualizePathStyle: { stroke: pathColor } });
       expect(us.say).toHaveBeenCalledWith("⚔️ attack");
     });
+    it("should *not* move to an enemy with way more health", () => {
+      const enemy = mockInstanceOf<Creep>({
+        my: false,
+        hits: 100000,
+        body: composeBody(1),
+        pos: {
+          x: 100,
+          y: 100
+        }
+      });
+
+      const us = mockInstanceOf<Creep>({
+        body: composeBody(2),
+        hits: 10,
+        memory: { role: "soldier", working: false },
+        store: { getFreeCapacity: () => 50 },
+        room: {
+          find: () => [enemy]
+        },
+        pos: {
+          x: 0,
+          y: 0,
+          findClosestByPath: () => enemy
+        },
+        moveTo: () => undefined,
+        say: () => undefined,
+        attack: () => ERR_NOT_IN_RANGE
+      });
+
+      routineSoldier(us);
+
+      expect(us.moveTo).toHaveBeenCalledTimes(0);
+      expect(us.say).toHaveBeenCalledTimes(0);
+    });
+    it("should *not* move to an enemy with way more body parts", () => {
+      const enemy = mockInstanceOf<Creep>({
+        my: false,
+        hits: 1,
+        body: composeBody(100),
+        pos: {
+          x: 100,
+          y: 100
+        }
+      });
+
+      const us = mockInstanceOf<Creep>({
+        body: composeBody(2),
+        hits: 10,
+        memory: { role: "soldier", working: false },
+        store: { getFreeCapacity: () => 50 },
+        room: {
+          find: () => [enemy]
+        },
+        pos: {
+          x: 0,
+          y: 0,
+          findClosestByPath: () => enemy
+        },
+        moveTo: () => undefined,
+        say: () => undefined,
+        attack: () => ERR_NOT_IN_RANGE
+      });
+
+      routineSoldier(us);
+
+      expect(us.moveTo).toHaveBeenCalledTimes(0);
+      expect(us.say).toHaveBeenCalledTimes(0);
+    });
     it("should *not* move to a stronger enemy", () => {
       const enemy = mockInstanceOf<Creep>({
         my: false,
         hits: 1000,
-        body: [
-          {
-            type: "attack",
-            hits: 100
-          },
-
-          {
-            type: "tough",
-            hits: 100
-          }
-        ],
+        body: composeBody(100),
         pos: {
           x: 100,
           y: 100
-        },
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        getActiveBodyparts: () => 25
+        }
       });
 
       const us = mockInstanceOf<Creep>({
-        body: [
-          {
-            type: "attack",
-            hits: 100
-          },
-          {
-            type: "tough",
-            hits: 100
-          }
-        ],
+        body: composeBody(2),
         hits: 10,
-        getActiveBodyparts: () => 10,
         memory: { role: "soldier", working: false },
         store: { getFreeCapacity: () => 50 },
         room: {
@@ -129,18 +153,8 @@ describe("Soldier role", () => {
       });
 
       const us = mockInstanceOf<Creep>({
-        body: [
-          {
-            type: "attack",
-            hits: 100
-          },
-          {
-            type: "tough",
-            hits: 100
-          }
-        ],
+        body: composeBody(2),
         hits: 10,
-        getActiveBodyparts: () => 10,
         memory: { role: "soldier", working: false },
         store: { getFreeCapacity: () => 50 },
         room: {
@@ -161,7 +175,7 @@ describe("Soldier role", () => {
       expect(us.moveTo).toHaveBeenCalledTimes(0);
       expect(us.say).toHaveBeenCalledTimes(0);
     });
-    it("should fight PowerCreeps with an advantage", () => {
+    it("should fight PowerCreeps whent it has the advantage", () => {
       const enemy = mockInstanceOf<PowerCreep>({
         my: false,
         hits: 100,
@@ -173,18 +187,8 @@ describe("Soldier role", () => {
       });
 
       const us = mockInstanceOf<Creep>({
-        body: [
-          {
-            type: "attack",
-            hits: 100
-          },
-          {
-            type: "tough",
-            hits: 100
-          }
-        ],
+        body: composeBody(2),
         hits: 1000,
-        getActiveBodyparts: () => 10,
         memory: { role: "soldier", working: false },
         store: { getFreeCapacity: () => 50 },
         room: {
@@ -219,18 +223,8 @@ describe("Soldier role", () => {
       });
 
       const us = mockInstanceOf<Creep>({
-        body: [
-          {
-            type: "attack",
-            hits: 100
-          },
-          {
-            type: "tough",
-            hits: 100
-          }
-        ],
+        body: composeBody(2),
         hits: 1000,
-        getActiveBodyparts: () => 10,
         memory: { role: "soldier", working: false },
         store: { getFreeCapacity: () => 50 },
         room: {
@@ -252,17 +246,7 @@ describe("Soldier role", () => {
       const enemy = mockInstanceOf<Creep>({
         my: false,
         hits: 10000,
-        body: [
-          {
-            type: "attack",
-            hits: 100
-          },
-          {
-            type: "tough",
-            hits: 100
-          }
-        ],
-        getActiveBodyparts: () => 200,
+        body: composeBody(200),
         pos: {
           x: 100,
           y: 100
@@ -270,18 +254,8 @@ describe("Soldier role", () => {
       });
 
       const us = mockInstanceOf<Creep>({
-        body: [
-          {
-            type: "attack",
-            hits: 100
-          },
-          {
-            type: "tough",
-            hits: 100
-          }
-        ],
+        body: composeBody(2),
         hits: 1000,
-        getActiveBodyparts: () => 10,
         memory: { role: "soldier", working: false },
         store: { getFreeCapacity: () => 50 },
         room: {
@@ -304,18 +278,8 @@ describe("Soldier role", () => {
     it("should transport when no enemies are in the room", () => {
       const us = mockInstanceOf<Creep>({
         my: true,
-        body: [
-          {
-            type: "attack",
-            hits: 100
-          },
-          {
-            type: "tough",
-            hits: 100
-          }
-        ],
+        body: composeBody(2),
         hits: 1000,
-        getActiveBodyparts: () => 10,
         memory: { role: "soldier", working: false },
         store: { getFreeCapacity: () => 0 },
         room: {
@@ -338,3 +302,19 @@ describe("Soldier role", () => {
     });
   });
 });
+
+function composeBody(parts: number): BodyPartDefinition[] {
+  const arr: BodyPartDefinition[] = [];
+  for (let i = 0; i < parts; i++) {
+    arr.push({
+      type: "attack",
+      hits: 100
+    });
+    arr.push({
+      type: "tough",
+      hits: 100
+    });
+  }
+
+  return arr;
+}
