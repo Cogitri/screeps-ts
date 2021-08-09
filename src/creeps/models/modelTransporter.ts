0253cb54a9f2bfc848286ae6990a289d77250e2b
janis.j.h.ciemnyjewski <janis.j.h.ciemnyjewski@student.fh-kiel.de>
2021-08-09 13:12:15 +0200

    #104 #79 reworked

--- src/creeps/models/modelTransporter.ts
+++ src/creeps/models/modelTransporter.ts
@@ -3,6 +3,6 @@ import spawnUtil from "utils/spawnUtil";
 
 const body = [WORK, MOVE, MOVE, CARRY, CARRY];
 
-export function spawnTransporter(spawn: StructureSpawn): void {
-  spawnUtil(CreepRoles.ROLE_TRANSPORTER, body, spawn);
+export function spawnTransporter(spawn: StructureSpawn): number {
+  return spawnUtil(CreepRoles.ROLE_TRANSPORTER, body, spawn);
 }
--- src/utils/spawnUtil.ts
+++ src/utils/spawnUtil.ts
@@ -1,8 +1,8 @@
 import { CreepRoles } from "./globalConsts";
 
-export default function (role: CreepRoles, bodyParts: BodyPartConstant[], spawn: StructureSpawn): void {
+export default function (role: CreepRoles, bodyParts: BodyPartConstant[], spawn: StructureSpawn): number {
   const name = `${role}${Game.time}${Math.trunc(Math.random() * 100)}`;
-  spawn.spawnCreep(bodyParts, name, {
+  return spawn.spawnCreep(bodyParts, name, {
     memory: { role, room: spawn.room.name, working: false, lockTask: false, target: null, currentTask: "none" }
   });
 }
