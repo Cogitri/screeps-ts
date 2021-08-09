11b83a1690952c8a2a102779a94cc54468cd37ce
Thorben Rolf <thorben.rolf@dampsoft.de>
2021-08-09 11:12:29 +0200

    #99 Spawnprozess anpassen
    - Spawnbefehle werden nicht mehr überschrieben
    -- Reihenfolge der Spawnbefehle angepasst
    - Anzahl der benötigten Creeps pro Rolle in globalConsts als Enum gesetzt

--- src/core/creepSpawn.ts
+++ src/core/creepSpawn.ts
@@ -1,28 +1,23 @@
-import { CreepRoles } from "utils/globalConsts";
+import { AmountPerRoles, CreepRoles } from "utils/globalConsts";
 import { spawnBuilder } from ".././creeps/models/modelBuilder";
 import { spawnHarvester } from ".././creeps/models/modelHarvester";
 import { spawnSoldier } from ".././creeps/models/modelSoldier";
 import { spawnTransporter } from ".././creeps/models/modelTransporter";
 
 export default function (spawn: StructureSpawn): void {
+  const harvester = _.filter(Game.creeps, creep => creep.memory.role === CreepRoles.ROLE_HARVESTER);
   const builders = _.filter(Game.creeps, creep => creep.memory.role === CreepRoles.ROLE_BUILDER);
-  // check number of creeps
-  if (builders.length < 3) {
-    spawnBuilder(spawn);
-  }
-  const harvesters = _.filter(Game.creeps, creep => creep.memory.role === CreepRoles.ROLE_HARVESTER);
-  // check number of creeps
-  if (harvesters.length < 3) {
-    spawnHarvester(spawn);
-  }
-  const soldiers = _.filter(Game.creeps, creep => creep.memory.role === CreepRoles.ROLE_SOLDIER);
-  // check number of creeps
-  if (soldiers.length < 1) {
-    spawnSoldier(spawn);
-  }
   const transporters = _.filter(Game.creeps, creep => creep.memory.role === CreepRoles.ROLE_TRANSPORTER);
+  const soldiers = _.filter(Game.creeps, creep => creep.memory.role === CreepRoles.ROLE_SOLDIER);
+
   // check number of creeps
-  if (transporters.length < 1) {
+  if (harvester.length < AmountPerRoles.AMOUNT_HARVESTER) {
+    spawnHarvester(spawn);
+  } else if (builders.length < AmountPerRoles.AMOUNT_BUILDER) {
+    spawnBuilder(spawn);
+  } else if (transporters.length < AmountPerRoles.AMOUNT_TRANSPORTER) {
     spawnTransporter(spawn);
+  } else if (soldiers.length < AmountPerRoles.AMOUNT_SOLDIER) {
+    spawnSoldier(spawn);
   }
 }
--- src/utils/globalConsts.ts
+++ src/utils/globalConsts.ts
@@ -4,6 +4,15 @@ export enum CreepRoles {
   ROLE_TRANSPORTER = "transporter",
   ROLE_SOLDIER = "soldier"
 }
+
+/** Amount of Creeps we want to spawn of each Role */
+export enum AmountPerRoles {
+  AMOUNT_HARVESTER = 3, // TODO Only spawn as many harvesters, as spots at a source.
+  AMOUNT_BUILDER = 3,
+  AMOUNT_TRANSPORTER = 1,
+  AMOUNT_SOLDIER = 1
+}
+
 export default {
   /** Minimum Tower Capacity */
   TARGET_TOWER_CAPACITY: 300
