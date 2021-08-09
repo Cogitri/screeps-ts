b428df88602d98e0c59fa994a32f0f682349756a
Rasmus Thomsen <oss@cogitri.dev>
2021-08-09 14:34:12 +0200

    Fix rebase, whoops

--- src/creeps/routines/routineWithdraw.ts
+++ src/creeps/routines/routineWithdraw.ts
@@ -2,25 +2,14 @@ import { PathColors } from "utils/globalConsts";
 import { movePath } from "./../../utils/vizPath";
 
 export default function (creep: Creep): void {
-<<<<<<< HEAD
-  const containers = creep.room.find(FIND_STRUCTURES, {
-    filter: s => s.structureType === STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] > 0
-  });
-
-  if (creep.withdraw(containers[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-    creep.say("📤  withdraw");
-    movePath(creep, containers[0], PathColors.PATHCOLOR_WITHDRAW);
-=======
-  const pathColor = "#33d6ff";
   const container = creep.pos.findClosestByPath(FIND_STRUCTURES, {
     filter: s => s.structureType === STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] > 0
   });
 
   if (container) {
     if (creep.withdraw(container, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-      creep.say("📤  withdraw");
-      movePath(creep, container, pathColor);
+      creep.say("📤 withdraw");
+      movePath(creep, container, PathColors.PATHCOLOR_WITHDRAW);
     }
->>>>>>> 6a5caa7 (#91 fixed find.room in tower, builder and withdraw)
   }
 }
