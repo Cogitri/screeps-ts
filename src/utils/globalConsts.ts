d509ce98eb138016fe8734508a7b221d225b6c8f
Lara Laskowsky <lara.laskowsky@student.fh-kiel.de>
2021-08-09 10:16:22 +0200

    #92: Add enum to global constants

--- src/utils/globalConsts.ts
+++ src/utils/globalConsts.ts
@@ -8,3 +8,15 @@ export default {
   /** Minimum Tower Capacity */
   TARGET_TOWER_CAPACITY: 300
 };
+export enum RoutinePathColors {
+  PATHCOLOR_PICKUPENERGY = "harvester",
+  PATHCOLOR_BUILDER = "#ffff33",
+  PATHCOLOR_ENERGIZE_TOWER = "harvester",
+  PATHCOLOR_FARM = "builder",
+  PATHCOLOR_HARVESTER = "harvester",
+  PATHCOLOR_SOLDIER = "builder",
+  PATHCOLOR_TOWER = "harvester",
+  PATHCOLOR_TRANSPORT = "builder",
+  PATHCOLOR_UPGRADE = "harvester",
+  PATHCOLOR_WITHDRAW = "builder"
+}
