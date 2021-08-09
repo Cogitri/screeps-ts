803e905fbb5dd6e36a9da2a6d6f39070e8974040
mikas <mika.schrader@student.fh-kiel.de>
2021-08-09 12:43:14 +0200

    #97 added implementation of buildRoadAroundSpawn

--- src/core/createConstructions.ts
+++ src/core/createConstructions.ts
@@ -1,10 +1,12 @@
-import { buildRoadToController, buildRoadToSource } from "construct/buildRoads";
+import { buildRoadAroundSpawn, buildRoadToController, buildRoadToSource } from "construct/buildRoads";
+
 import createContainer from "construct/createContainer";
 import createRampart from "construct/createRampart";
 
 export default function (spawn: StructureSpawn): void {
   buildRoadToController(spawn);
   buildRoadToSource(spawn);
+  buildRoadAroundSpawn(spawn);
   createRampart(spawn);
   createContainer(spawn);
 }
