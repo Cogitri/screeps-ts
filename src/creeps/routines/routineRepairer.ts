4f272855138cb07a1b1ddcf4d37eb2f210106079
katharina.d.sprotte <katharina.d.sprotte@student.fh-kiel.de>
2021-08-09 11:31:59 +0200

    #91 fixed find room in repairer

--- src/creeps/routines/routineRepairer.ts
+++ src/creeps/routines/routineRepairer.ts
@@ -1,21 +1,25 @@
+import { movePath } from "utils/vizPath";
+// eslint-disable-next-line sort-imports
 import checkCreepCapacity from "./checkCreepCapacity";
 import routineFarm from "./routineFarm";
 import routineTransporter from "./routineTransporter";
 
 export default function (creep: Creep): void {
-  const damagedStructure = creep.room.find(FIND_STRUCTURES, {
+  // check for all damaged structures
+  const damagedStructures = creep.room.find(FIND_STRUCTURES, {
     filter: s =>
       (s.structureType === STRUCTURE_WALL && s.hits < 0.1 * s.hitsMax) ||
       (s.structureType !== STRUCTURE_WALL && s.hits < 0.69 * s.hitsMax)
   });
 
+  // check for all construction sites
   const targets = creep.room.find(FIND_CONSTRUCTION_SITES);
 
   if (!creep.memory.working) {
     if (checkCreepCapacity(creep)) {
       routineFarm(creep);
-    } else if (checkDamagedStructure(damagedStructure[0])) {
-      repair(creep, damagedStructure[0]);
+    } else if (checkDamagedStructure(damagedStructures[0])) {
+      repair(creep);
     } else if (checkConstructionSite(targets[0])) {
       build(creep, targets[0]);
     } else {
@@ -36,13 +40,25 @@ function build(creep: Creep, target: ConstructionSite): void {
 }
 
 // Function to start repairing
-function repair(creep: Creep, damagedStructure: AnyStructure): void {
+function repair(creep: Creep): void {
+  // check for closes damaged structure
+  const damagedStructure = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+    filter: s =>
+      (s.structureType === STRUCTURE_WALL && s.hits < 0.001 * s.hitsMax) ||
+      (s.structureType === STRUCTURE_RAMPART && s.hits < 0.01 * s.hitsMax) ||
+      (s.structureType === STRUCTURE_ROAD && s.hits < 0.25 * s.hitsMax) ||
+      (s.structureType !== STRUCTURE_RAMPART &&
+        s.structureType !== STRUCTURE_WALL &&
+        s.structureType !== STRUCTURE_ROAD &&
+        s.hits < 0.69 * s.hitsMax)
+  });
   const pathColor = "#ffff33";
-
-  if (creep.repair(damagedStructure) === ERR_NOT_IN_RANGE) {
-    creep.memory.lockTask = true;
-    creep.say("🛠️ repair");
-    creep.moveTo(damagedStructure, { visualizePathStyle: { stroke: pathColor } });
+  if (damagedStructure) {
+    if (creep.repair(damagedStructure) === ERR_NOT_IN_RANGE) {
+      creep.memory.lockTask = true;
+      creep.say("🛠️ repair");
+      movePath(creep, damagedStructure, pathColor);
+    }
   }
 }
 
