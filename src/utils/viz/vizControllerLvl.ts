88909d5db7a151f43d041bad142f9f8036b9906e
katharina.d.sprotte <katharina.d.sprotte@student.fh-kiel.de>
2021-08-11 11:40:36 +0200

    #128 added message for max level fixes #128

--- src/utils/viz/vizControllerLvl.ts
+++ src/utils/viz/vizControllerLvl.ts
@@ -9,12 +9,16 @@ export function visualizeControllerProgress(room: Room): void {
     const yCoordinate = roomController.pos.y;
 
     if (global.textViz) {
-      room.visual.text(
-        `${((roomController.progress / roomController.progressTotal) * 100).toFixed(2)}% done`,
-        xCoordinate,
-        yCoordinate + 1.5,
-        { color: "white", font: 0.6 }
-      );
+      if (roomController.level < 8) {
+        room.visual.text(
+          `${((roomController.progress / roomController.progressTotal) * 100).toFixed(2)}% done`,
+          xCoordinate,
+          yCoordinate + 1.5,
+          { color: "white", font: 0.6 }
+        );
+      } else {
+        room.visual.text(`Max lvl. achieved 🎉`, xCoordinate, yCoordinate + 1.5, { color: "white", font: 0.6 });
+      }
     } else {
       room.visual.text("", xCoordinate, yCoordinate);
     }
