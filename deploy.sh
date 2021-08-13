5634217561b4dcc6b33206fb09c396f0ff66a7f3
TimFB <56914732+TibiIius@users.noreply.github.com>
2021-08-13 10:06:15 +0200

    fixup! fixup! fixup!  #57: fix cd

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 set -e
 
+cp screeps.sample.json screeps.json
+
 # Call when the pipeline should deploy to PO's Screeps account
 if [ -z $SCREEPS_WEB_DEPLOY ]; then
-  cp screeps.sample.json screeps.json
   sed -i "s/\"YOUR_TOKEN\"/\"$SCREEPS_MASTER_DEPLOY\"/g" ./screeps.json
-  yarn
-  yarn push-main
 # Call if pipeline was triggered from web frontend
 else
-  cp screeps.sample.json screeps.json
   sed -i "s/\"YOUR_TOKEN\"/\"$SCREEPS_WEB_DEPLOY\"/g" ./screeps.json
-  yarn
-  yarn push-main
 fi
+
+cat screeps.json
+
+yarn
+yarn push-main
 
 cat screeps.json
